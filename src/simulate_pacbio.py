05ae3d7393b1e279b58685b61cafee583d704a7b
Andrey Prjibelski <andrewprzh@gmail.com>
2021-05-05 12:21:32 +0300

    thread option for pb simulation

--- src/simulate_pacbio.py
+++ src/simulate_pacbio.py
@@ -15,13 +15,14 @@ def simulate_pacbio(args, read_count=1000):
     isoseqsim = os.path.join(src_path, "isoseqsim/bin/isoseqsim.py")
     param_dir = os.path.join(src_path, "isoseqsim/utilities/")
     ref_prefix = args.reference_prefix
+    keep_isoforom_ids = ' --keep_isoform_ids' if args.keep_isoform_ids else ''
     result = subprocess.run([isoseqsim, "--cpu", str(args.threads), "--tempdir", args.tmp_dir,
                              "--annotation", ref_prefix + ".annotation.gtf",
                              "--genome", ref_prefix + ".genome.fasta", "--expr", args.counts,
                              "--c5", os.path.join(param_dir, "5_end_completeness.PacBio-Sequel.tab"),
                              "--c3", os.path.join(param_dir, "3_end_completeness.PacBio-Sequel.tab"),
                              "--es", "0.004", "--ei", "0.006", "--ed", "0.006",
-                             "--read_number", str(read_count / 1000000.0), "--polya",
+                             "--read_number", str(read_count / 1000000.0), "--polya", keep_isoforom_ids,
                              "--transcript", os.path.join(args.output, "PacBio.simulated.tsv"),
                              "-o", os.path.join(args.output, "PacBio.simulated")])
 
